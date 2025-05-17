8c8bf3d0cdb7d66275dd3861186af28da42ebf48
catherinewuyep-at-901044882194 <AIk1wnTTmD4HJphZCOBvtgdYTEeDf+ujxTxnBMUFaM1n0PbUQ1yT0BNfnNc=>
2025-05-17 14:04:15 +0100

    QUERY OPTIMIZATION

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -2,19 +2,28 @@
 
 SELECT 
     b.id AS booking_id,
+    b.booking_date,
+    
     u.id AS user_id,
     u.name AS user_name,
     u.email AS user_email,
+
     p.id AS property_id,
     p.name AS property_name,
     p.location,
+
     pay.id AS payment_id,
     pay.amount,
-    pay.status
+    pay.payment_date
+
 FROM bookings b
 JOIN users u ON b.user_id = u.id
 JOIN properties p ON b.property_id = p.id
-JOIN payments pay ON b.payment_id = pay.id;
+JOIN payments pay ON b.payment_id = pay.id
+
+WHERE b.booking_date >= '2024-01-01'
+AND pay.status = 'completed';
+
 -- This query retrieves booking details along with user, property, and payment information.
 -- It joins the bookings, users, properties, and payments tables to provide a comprehensive view of the booking process.
 
